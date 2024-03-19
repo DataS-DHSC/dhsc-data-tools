cf88bd6c09193cf42745a7d9bf168e8f695651bf
RichardPordan <105643799+richardpordan@users.noreply.github.com>
2024-03-19 12:21:14 +0000

    pylint suggested changes

--- src/dhsc_data_tools/dac_odbc.py
+++ src/dhsc_data_tools/dac_odbc.py
@@ -1,67 +1,72 @@
+"""Module dac_odbc allows to interact with DAC SQL endpoints."""
+
 import os
 from msal import PublicClientApplication
 import pyodbc
 from pypac import pac_context_for_url
 from dhsc_data_tools.keyvault import kvConnection
 
+
 def connect(environment: str = "prod"):
+    """Allows to connect to data within the DAC, and query it using SQL queries.
 
-    '''Allows to connect to data within the DAC, and query it using SQL queries.
+    Parameters: an environment argument, which defaults to "prod".
+    Must be one of "dev", "qa", "test", "prod".
 
-    Parameters: an environment argument, which defaults to "prod". Must be one of "dev", "qa", "test", "prod".
-
-    Requires: 
-    TENANT_NAME environment variable. 
-    Simba Spark ODBC Driver is required. Request the latter through IT portal, install through company portal.
+    Requires:
+    TENANT_NAME environment variable.
+    Simba Spark ODBC Driver is required.
+    Request the latter through IT portal, install through company portal.
 
     Returns: connection object.
-    '''
+    """
 
-    print("\nUser warning: Expect two authentication pop-up windows,\nwhich may ask you to authenticate and then \n confirm 'Authentication complete.'")
-    
+    print("User warning: Expect two authentication pop-up windows.")
+    print(
+        "These may ask you to authenticate and then confirm 'Authentication complete.'"
+    )
+
     # Using Azure CLI app ID
     client_id = "04b07795-8ddb-461a-bbee-02f9e1bf7b46"
     try:
         tenant_name = os.environ["TENANT_NAME"]
-    except KeyError:
-        raise KeyError("TENANT_NAME environment variable not found. \nPlease load this as it is required to connect to the DAC.")
+    except KeyError as exc:
+        raise KeyError("TENANT_NAME environment variable not found.") from exc
 
     # Do not modify this variable. It represents the programmatic ID for
     # Azure Databricks along with the default scope of '/.default'.
-    scope = [ '2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default' ]
+    scope = ["2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default"]
 
     # create client
     app = PublicClientApplication(
-        client_id = client_id,
-        authority = "https://login.microsoftonline.com/" + tenant_name
-        )
+        client_id=client_id,
+        authority="https://login.microsoftonline.com/" + tenant_name,
+    )
 
     # acquire token
     with pac_context_for_url("https://www.google.co.uk/"):
-        token = app.acquire_token_interactive(
-            scopes = scope
-        )
+        token = app.acquire_token_interactive(scopes=scope)
 
     # establish keyvault connection
-    KVC = kvConnection(environment)
+    kvc = kvConnection(environment)
 
     # retrieve relevant key vault secrets
-    with pac_context_for_url(KVC.KVUri):
-        host_name = KVC.get_secret("dac-db-host")
-        ep_path = KVC.get_secret("dac-sql-endpoint-http-path")
+    with pac_context_for_url(kvc.kv_uri):
+        host_name = kvc.get_secret("dac-db-host")
+        ep_path = kvc.get_secret("dac-sql-endpoint-http-path")
 
     # establish connection
     conn = pyodbc.connect(
-        "Driver=Simba Spark ODBC Driver;" +
-        f"Host={host_name};" + # from keyvaults
-        "Port=443;" +
-        f"HTTPPath={ep_path};" + # from keyvaults 
-        "SSL=1;" +
-        "ThriftTransport=2;" +
-        "AuthMech=11;" +
-        "Auth_Flow=0;" +
-        f"Auth_AccessToken={token['access_token']}",
-        autocommit = True
+        "Driver=Simba Spark ODBC Driver;"
+        + f"Host={host_name};"
+        + "Port=443;"  # from keyvaults
+        + f"HTTPPath={ep_path};"
+        + "SSL=1;"  # from keyvaults
+        + "ThriftTransport=2;"
+        + "AuthMech=11;"
+        + "Auth_Flow=0;"
+        + f"Auth_AccessToken={token['access_token']}",
+        autocommit=True,
     )
 
     return conn
--- src/dhsc_data_tools/remote_compute.py
+++ src/dhsc_data_tools/remote_compute.py
@@ -1,47 +1,50 @@
-import os
+"""Module allows to run code on Azure Databricks clusters."""
+
 import configparser
 from databricks.connect import DatabricksSession
 
-def connect_cluster(profile: str = "DEFAULT", file: str = r'.config', cluster_uid: str = ""):
-    '''Establishes a connection with a databricks compute cluster.
-    
+
+def connect_cluster(
+    profile: str = "DEFAULT", file: str = r".config", cluster_uid: str = ""
+):
+    """Establishes a connection with a databricks compute cluster.
+
     Requires:
-    It relies on a `.config` file in the working directory, containing HOST, TOKEN, CLUSTER_ID:
+    It relies on a `.config` file in the working directory, containing:
     [<profile-name>]
     HOST=xxx
     TOKEN=xxx
     CLUSTER_ID=xxx
 
-    You can also pass a cluster_id parameter manually, which will override the config file parameter.
+    NB: You can also pass a cluster_id parameter manually,
+        which will override the config file parameter.
 
-    Parameters: 
-    profile (config profile name defaults to "DEFAULT"),  
+    Parameters:
+    profile (config profile name defaults to "DEFAULT"),
     file ()
 
     Returns: a spark instance.
-    '''
+    """
 
-    #Get configuration
+    # Get configuration
     config = configparser.ConfigParser()
-    config.read_file(open(file))
-    
-    keys = {"host_url":config.get(profile, 'HOST'), 
-            "token":config.get(profile, 'TOKEN'), 
-            "cluster_id":config.get(profile, 'CLUSTER_ID')}
-    
-    #Connect to cluster; if cluster_uid argument was given,
+    config.read(file, encoding="utf8")
+
+    keys = {
+        "host_url": config.get(profile, "HOST"),
+        "token": config.get(profile, "TOKEN"),
+        "cluster_id": config.get(profile, "CLUSTER_ID"),
+    }
+
+    # Connect to cluster; if cluster_uid argument was given,
     # override config file.
-    if cluster_uid=="":
+    if cluster_uid == "":
         spark = DatabricksSession.builder.remote(
-        host       = keys['host_url'], 
-        token      = keys['token'], 
-        cluster_id = keys['cluster_id'] 
+            host=keys["host_url"], token=keys["token"], cluster_id=keys["cluster_id"]
         ).getOrCreate()
     else:
         spark = DatabricksSession.builder.remote(
-        host       = keys['host_url'], 
-        token      = keys['token'], 
-        cluster_id = cluster_uid
+            host=keys["host_url"], token=keys["token"], cluster_id=cluster_uid
         ).getOrCreate()
 
     # Return a spark instance
