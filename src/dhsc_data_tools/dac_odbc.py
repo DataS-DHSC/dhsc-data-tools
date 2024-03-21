bae913df94928aae67221ffcf95077e0ebb18ef2
RichardPordan <105643799+richardpordan@users.noreply.github.com>
2024-03-21 13:31:29 +0000

    src code changes to allow silent auth through caching

--- src/dhsc_data_tools/dac_odbc.py
+++ src/dhsc_data_tools/dac_odbc.py
@@ -1,9 +1,9 @@
 """Module dac_odbc allows to interact with DAC SQL endpoints."""
 
 import os
-from msal import PublicClientApplication
 import pyodbc
 from pypac import pac_context_for_url
+from azure.identity import SharedTokenCacheCredential, TokenCachePersistenceOptions
 from dhsc_data_tools.keyvault import kvConnection
 
 
@@ -14,38 +14,23 @@ def connect(environment: str = "prod"):
     Must be one of "dev", "qa", "test", "prod".
 
     Requires:
-    TENANT_NAME environment variable.
+    DAC_TENANT environment variable to be loaded.
     Simba Spark ODBC Driver is required.
     Request the latter through IT portal, install through company portal.
 
     Returns: connection object.
     """
 
-    print("User warning: Expect two authentication pop-up windows.")
-    print(
-        "These may ask you to authenticate and then confirm 'Authentication complete.'"
-    )
+    print("User warning: Expect an authentication pop-up window.")
+    print("You will only be asked to authenticate once the first time.")
 
     # Using Azure CLI app ID
     client_id = "04b07795-8ddb-461a-bbee-02f9e1bf7b46"
-    try:
-        tenant_name = os.environ["DAC_TENANT"]
-    except KeyError as exc:
-        raise KeyError("DAC_TENANT environment variable not found.") from exc
-
-    # Do not modify this variable. It represents the programmatic ID for
-    # Azure Databricks along with the default scope of '/.default'.
-    scope = ["2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default"]
-
-    # create client
-    app = PublicClientApplication(
-        client_id=client_id,
-        authority="https://login.microsoftonline.com/" + tenant_name,
-    )
-
-    # acquire token
-    with pac_context_for_url("https://www.google.co.uk/"):
-        token = app.acquire_token_interactive(scopes=scope)
+    tenant_name = os.getenv("DAC_TENANT")
+    if tenant_name:
+        pass
+    else:
+        raise KeyError("DAC_TENANT environment variable not found.")
 
     # establish keyvault connection
     kvc = kvConnection(environment)
@@ -54,6 +39,18 @@ def connect(environment: str = "prod"):
     with pac_context_for_url(kvc.kv_uri):
         host_name = kvc.get_secret("dac-db-host")
         ep_path = kvc.get_secret("dac-sql-endpoint-http-path")
+
+    # Do not change the value of the scope parameter. It represents the programmatic ID 
+    # for Azure Databricks (2ff814a6-3304-4ab8-85cb-cd0e6f879c1d) along with the default 
+    # scope (/.default, URL-encoded as %2f.default).
+    scope = "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default"
+    
+    # Do not modify `allow_unencrypted_storage` parameter - which defaults to False
+    cache_options = TokenCachePersistenceOptions()
+
+    # Get auth token
+    credential = SharedTokenCacheCredential(cache_persistence_options=cache_options)
+    token = credential.get_token(scope)
 
     # establish connection
     conn = pyodbc.connect(
@@ -65,7 +62,7 @@ def connect(environment: str = "prod"):
         + "ThriftTransport=2;"
         + "AuthMech=11;"
         + "Auth_Flow=0;"
-        + f"Auth_AccessToken={token['access_token']}",
+        + f"Auth_AccessToken={token.token}",
         autocommit=True,
     )
 
--- src/dhsc_data_tools/keyvault.py
+++ src/dhsc_data_tools/keyvault.py
@@ -3,6 +3,8 @@
 import os
 from azure.keyvault.secrets import SecretClient
 from azure.identity import InteractiveBrowserCredential
+from azure.identity import TokenCachePersistenceOptions
+from azure.identity import SharedTokenCacheCredential
 
 
 class kvConnection:
@@ -18,29 +20,52 @@ class kvConnection:
 
     Returns: Azure Keyvault Connection object.
     """
+    
+    print("User warning: Expect an authentication pop-up window.")
+    print("You will only be asked to authenticate once the first time.")
 
     def __init__(self, environment: str = "prod"):
+
         if environment.upper() in ["DEV", "TEST", "QA", "PROD"]:
             temp_vault_name = os.getenv("KEY_VAULT_NAME")
-            try:
+            if temp_vault_name:
+                # KEY_VAULT_NAME must include {env} for .format method
                 self.vault_name = temp_vault_name.format(env=environment.lower())
-            except AttributeError as exc:
-                raise AttributeError(
+            else:
+                raise KeyError(
                     "KEY_VAULT_NAME environment variable not found."
-                ) from exc
+                )
             if self.vault_name:
                 self.kv_uri = f"https://{self.vault_name}.vault.azure.net"
             else:
-                raise KeyError("KEY_VAULT_NAME environment variable not found.")
+                raise KeyError(
+                    "KEY_VAULT_NAME environment variable not found."
+                    )
         else:
             raise ValueError(
                 "Environment name argument must be one of 'dev', 'test', 'qa', 'prod'."
             )
 
-        self.credential = InteractiveBrowserCredential(
-            client_id="04b07795-8ddb-461a-bbee-02f9e1bf7b46",
-            additionally_allowed_tenants="*",
-        )
+        # Do not change the value of the scope parameter. It represents the programmatic ID 
+        # for Azure Databricks (2ff814a6-3304-4ab8-85cb-cd0e6f879c1d) along with the default 
+        # scope (/.default, URL-encoded as %2f.default).
+        scope = "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default"
+        
+        # Do not add/modify `allow_unencrypted_storage` parameter,
+        # this defaults to False as intended.
+        cache_options = TokenCachePersistenceOptions()
+
+        # Establish client
+        try:
+            self.credential = SharedTokenCacheCredential(cache_persistence_options=cache_options)
+            token = self.credential.get_token(scope)
+        except Exception as e:   
+            self.credential = InteractiveBrowserCredential(
+                client_id="04b07795-8ddb-461a-bbee-02f9e1bf7b46",
+                cache_persistence_options = cache_options,
+                additionally_allowed_tenants=['*']
+            )
+            token = self.credential.get_token(scope) 
 
         self.client = SecretClient(vault_url=self.kv_uri, credential=self.credential)
 
